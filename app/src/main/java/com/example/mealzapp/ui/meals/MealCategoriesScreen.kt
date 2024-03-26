71ccbc4ad98324418f1cd46d7a66c224234f2a99
Muneeraa96 <Muneera290796@gmail.com>
2024-03-26 23:59:07 +0100

    expandable item list implemented

--- app/src/main/java/com/example/mealzapp/ui/meals/MealCategoriesScreen.kt
+++ app/src/main/java/com/example/mealzapp/ui/meals/MealCategoriesScreen.kt
@@ -1,6 +1,9 @@
 package com.example.mealzapp.ui.meals
 
+import android.graphics.drawable.Icon
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -11,11 +14,21 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberImagePainter
@@ -43,6 +56,7 @@ fun MealsCategoriesScreen(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun MealCategory(it: MealResponse) {
+    var isExpanded by remember{ mutableStateOf(false) }
     Card(
         shape = RoundedCornerShape(8.dp),
         elevation = 2.dp,
@@ -51,19 +65,38 @@ fun MealCategory(it: MealResponse) {
             .padding(top = 16.dp)
     ) {
 
-        Row {
+        Row(modifier = Modifier.animateContentSize()) {
             Image(
                 painter = rememberImagePainter(it.imageUrl),
                 contentDescription = null,
                 modifier = Modifier
                     .size(88.dp)
                     .padding(4.dp)
+                    .align(Alignment.CenterVertically)
             )
-            Column(modifier =
-            Modifier.align(alignment = Alignment.CenterVertically).padding(16.dp),
-                ) {
+            Column(
+                modifier =
+                Modifier
+                    .align(alignment = Alignment.CenterVertically)
+                    .fillMaxWidth(0.8f)
+                    .padding(16.dp),
+            ) {
                 Text(text = it.name, style = MaterialTheme.typography.h6)
+                CompositionLocalProvider(value = LocalContentAlpha provides ContentAlpha.high) {
+                    Text(
+                        text = it.description, style = MaterialTheme.typography.body2,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = if (isExpanded) 10 else 4
+                    )
+                }
             }
+            Image(
+                imageVector = if(isExpanded)Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = "Keyboard Arrow Up",
+                modifier = Modifier.align(if(isExpanded)Alignment.Bottom else Alignment.CenterVertically).padding(16.dp).clickable {
+                    isExpanded = !isExpanded
+                }
+            )
         }
 
     }
