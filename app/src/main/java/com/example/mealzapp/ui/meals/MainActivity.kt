12b34e5b545a949b286915904a1223aa48eaa7f9
Muneeraa96 <Muneera290796@gmail.com>
2024-04-26 01:32:47 +0200

    comment added

--- app/src/main/java/com/example/mealzapp/ui/meals/MainActivity.kt
+++ app/src/main/java/com/example/mealzapp/ui/meals/MainActivity.kt
@@ -31,6 +31,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MealzAppTheme {
+                //start defining navigation
                 mealsNavigation()
             }
         }
